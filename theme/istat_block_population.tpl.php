a6fdff323c3d04cc6b0e56fe1bbbebccf35a68a3
Paolo Mainardi <paolomainardi@gmail.com>
2011-02-15 01:10:26 +0100

    first commit

---
+++ theme/istat_block_population.tpl.php
@@ -0,0 +1,8 @@
+<iframe 
+  width="<?php print $width ?>" height="<?php print $height ?>" 
+  src="<?php print $url ?>?codice=<?php print $code ?>" 
+  scrolling="no"
+  frameborder="0"
+  marginwidth="0" 
+  marginheight="0" >
+</iframe>		
